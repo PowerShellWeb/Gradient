2a04c341599671ddffbb3bc4d6769746a53fe217
StartAutomating <james.brundage@start-automating.com>
2025-12-29 12:25:23 -0800

    feat: Gradient Action ( Fixes #7 )

---
+++ Build/Gradient.GitHubAction.PSDevOps.ps1
@@ -0,0 +1,10 @@
+﻿#requires -Module PSDevOps
+Import-BuildStep -SourcePath (
+    Join-Path $PSScriptRoot 'GitHub'
+) -BuildSystem GitHubAction
+
+$PSScriptRoot | Split-Path | Push-Location
+
+New-GitHubAction -Name "UseGradient" -Description 'Gradient Generator' -Action GradientAction -Icon chevron-right -OutputPath .\action.yml
+
+Pop-Location
