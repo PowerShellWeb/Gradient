219b215b8d4da9534ab56c9770ecb20ff0006d88
StartAutomating <james.brundage@start-automating.com>
2026-01-11 21:33:08 -0800

    feat: Gradient.ToString flexibility ( Fixes #23 )
    Also expanding tests

--- Gradient.tests.ps1
+++ Gradient.tests.ps1
@@ -11,4 +11,76 @@ describe Gradient {
         )
         $($gradients | Gradient) | Should -Match 'linear-gradient[\s\S]+?,'
     }
+    it 'Can stack gradients' {
+        
+        $gradients = @(
+            gradient repeating-radial  @(
+                'circle closest-side at 0.01% 0.01%'
+                'color-mix(in srgb, red 50%, transparent) 1rem'
+                'color-mix(in srgb, green 50%, transparent) 2rem'
+                'color-mix(in srgb, blue 50%, transparent) 3rem'
+            )
+
+            gradient repeating-radial  @(
+                'circle closest-side at 99.99% 0.01%'
+                'color-mix(in srgb, red 50%, transparent) 1rem'
+                'color-mix(in srgb, green 50%, transparent) 2rem'
+                'color-mix(in srgb, blue 50%, transparent) 3rem'   
+            )
+        ) 
+        
+        "$($gradients | gradient)" -match 'repeating-radial-gradient[\s\S]+?repeating-radial-gradient'                     
+    }
+
+    it 'Can stack multiple types' {
+        $gradients = @(
+            gradient repeating-radial  @(
+                'circle closest-side at 0.01% 0.01%'
+                'color-mix(in srgb, red 33%, transparent) 1rem'
+                'color-mix(in srgb, green 33%, transparent) 2rem'
+                'color-mix(in srgb, blue 33%, transparent) 3rem'
+            )
+
+            gradient repeating-linear @(
+                '135deg'
+                'color-mix(in srgb, red 33%, transparent) 1rem'
+                'color-mix(in srgb, green 33%, transparent) 2rem'
+                'color-mix(in srgb, blue 33%, transparent) 3rem'   
+            )
+
+            gradient repeating-conic  @(
+                'from 180deg at 99.99% 0.01%'
+                'color-mix(in srgb, red 33%, transparent) 0% 1%'
+                'color-mix(in srgb, green 33%, transparent) 1% 2%'
+                'color-mix(in srgb, blue 33%, transparent) 2% 3%'   
+            )            
+        ) | gradient
+                
+        "$($gradients)" | Should -Match 'repeating-radial-gradient'
+        "$($gradients)" | Should -Match 'repeating-linear-gradient'
+        "$($gradients)" | Should -Match 'repeating-conic-gradient'
+
+        $gradients.ToString("html") > ./mutli.html
+    }
+    it 'Can be clever with ToString' {
+        gradient repeating-radial 'red 1rem', 'green 2rem', 'blue 3rem'
+        
+        $gradient = @(
+            gradient repeating-radial  @(
+                'circle closest-side at 0.01% 0.01%'
+                'color-mix(in srgb, red 50%, transparent) 1rem'
+                'color-mix(in srgb, green, transparent) 2rem'
+                'color-mix(in srgb, blue, transparent) 3rem'    
+            )
+
+            gradient repeating-radial  @(
+                'circle closest-side at 99.99% 0.01%'
+                'color-mix(in srgb, red 50%, transparent) 1rem'
+                'color-mix(in srgb, green 50%, transparent) 2rem'
+                'color-mix(in srgb, blue 50%, transparent) 3rem'
+            )
+        ) | Gradient
+
+        $gradient.ToString("html") > ./test.html 
+    }
 }
--- Types/Gradient/ToString.ps1
+++ Types/Gradient/ToString.ps1
@@ -3,5 +3,30 @@
     Stringifies the gradient
 .DESCRIPTION
     Gets the gradient as a string.  By default, returns the CSS gradient.
+
+    If any arguments are passed and are properties of this object,
+    they will be returned as strings.
+.EXAMPLE
+    "$(gradient '#4488ff' '#224488')"
+.EXAMPLE
+    (gradient '#4488ff' '#224488').ToString("html")
 #>
+param()
+
+if ($args) {
+    $anymatching = @(foreach ($arg in $args) {
+        $thisArg = $this.$arg
+        if ($thisArg) {
+            if ($thisArg -is [xml]) {
+                $thisArg.Outerxml
+            } else {
+                $thisArg
+            }
+        }
+    })
+    if ($anymatching) {
+        return ($anymatching -as 'string[]' -join [Environment]::NewLine)
+    }
+}
+
 return "$($this.CSS)"
